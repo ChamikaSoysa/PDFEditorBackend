9d07e28cd638c12f927d1ec23c03ccd353875f0e
ChamikaSoysa <cdssoysa@gmail.com>
2025-09-28 14:38:48 +0530

    setup the methods on services by the inherited methods in interface

---
+++ PDFBackend/Services/Services/FileStorageService.cs
@@ -0,0 +1,24 @@
+﻿using PDFBackend.Extensions;
+using PDFBackend.Interfaces;
+
+namespace PDFBackend.Services.Services
+{
+    public class FileStorageService: IFileStorageService
+    {
+        public async Task<string> SaveFileAsync(IFormFile file, string folder = "uploads")
+        {
+
+        }
+
+        public async Task<byte[]> ReadFileAsync(string filePath)
+        {
+
+        }
+
+        public async Task DeleteFileAsync(string filePath)
+        {
+
+        }
+
+    }
+}
