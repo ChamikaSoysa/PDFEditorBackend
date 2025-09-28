78cda8c4c6615ec8fbfbe68d7177f6ad35a062ae
ChamikaSoysa <cdssoysa@gmail.com>
2025-09-28 14:40:26 +0530

    setting of validations in save file

--- PDFBackend/Services/Services/FileStorageService.cs
+++ PDFBackend/Services/Services/FileStorageService.cs
@@ -8,6 +8,12 @@ namespace PDFBackend.Services.Services
         public async Task<string> SaveFileAsync(IFormFile file, string folder = "uploads")
         {
 
+            if (file == null || file.Length == 0)
+                throw new ArgumentException("File is empty");
+
+            var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+            if (ext != ".pdf")
+                throw new ArgumentException("Only PDF files allowed");
         }
 
         public async Task<byte[]> ReadFileAsync(string filePath)
