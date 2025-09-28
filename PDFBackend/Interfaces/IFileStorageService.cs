b5ecce72960f3906295f31b9c759fef83233e349
ChamikaSoysa <cdssoysa@gmail.com>
2025-09-28 14:35:34 +0530

    setting the Interfaces signatures to read, save, and delete the pdf file

---
+++ PDFBackend/Interfaces/IFileStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace PDFBackend.Interfaces
+{
+    public interface IFileStorageService
+    {
+        Task<byte[]> ReadFileAsync(string filePath);
+        Task<string> SaveFileAsync(IFormFile file, string folder = "uploads");
+        Task DeleteFileAsync(string filePath);
+    }
+}
