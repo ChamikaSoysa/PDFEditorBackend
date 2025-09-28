73cdf613c765887e63a721b0b4babaaf116a4844
ChamikaSoysa <cdssoysa@gmail.com>
2025-09-28 14:44:06 +0530

    Sanitizing the FileName and set to Directory to copy and return the saved path

--- PDFBackend/Services/Services/FileStorageService.cs
+++ PDFBackend/Services/Services/FileStorageService.cs
@@ -5,6 +5,7 @@ namespace PDFBackend.Services.Services
 {
     public class FileStorageService: IFileStorageService
     {
+        private readonly string _baseTempPath = Path.Combine(Path.GetTempPath(), "PdfBackend");
         public async Task<string> SaveFileAsync(IFormFile file, string folder = "uploads")
         {
 
@@ -14,6 +15,16 @@ namespace PDFBackend.Services.Services
             var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
             if (ext != ".pdf")
                 throw new ArgumentException("Only PDF files allowed");
+
+            var safeName = Sanitizer.SanitizeFileName(Path.GetFileNameWithoutExtension(file.FileName)) + ext;
+            var dir = Path.Combine(_baseTempPath, folder, Guid.NewGuid().ToString());
+            Directory.CreateDirectory(dir);
+
+            var fullPath = Path.Combine(dir, safeName);
+            using var stream = new FileStream(fullPath, FileMode.Create);
+            await file.CopyToAsync(stream);
+            return fullPath;
+
         }
 
         public async Task<byte[]> ReadFileAsync(string filePath)
