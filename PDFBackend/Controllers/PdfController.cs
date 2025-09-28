6747facd74a3386a57a5fa92f5d349cd09351dc8
ChamikaSoysa <cdssoysa@gmail.com>
2025-09-28 15:17:55 +0530

    Setting up the controller for pdf backend

---
+++ PDFBackend/Controllers/PdfController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PDFBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PdfController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
