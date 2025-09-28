717b74b6790d0710783a0895f212db19cac131df
ChamikaSoysa <cdssoysa@gmail.com>
2025-09-28 15:21:11 +0530

    setting up the constructor and service injection to pdfcontroller

--- PDFBackend/Controllers/PdfController.cs
+++ PDFBackend/Controllers/PdfController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PDFBackend.Interfaces;
 
 namespace PDFBackend.Controllers
 {
@@ -6,6 +7,18 @@ namespace PDFBackend.Controllers
     [Route("api/[controller]")]
     public class PdfController : Controller
     {
+
+        private readonly IFileStorageService _fileStorage;
+        private readonly IPdfService _pdfService;
+        private readonly ILogger<PdfController> _logger;
+
+        public PdfController(IFileStorageService fileStorage, IPdfService pdfService, ILogger<PdfController> logger)
+        {
+            _fileStorage = fileStorage;
+            _pdfService = pdfService;
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
