d235a6aed230562b298f34f997a9cdca286aaaf2
ChamikaSoysa <cdssoysa@gmail.com>
2025-09-28 15:48:37 +0530

    Add Text Validators of Dto

---
+++ PDFBackend/Validators/TextEditValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using PDFBackend.DTOs;
+
+namespace PDFBackend.Validators
+{
+    public class TextEditValidator : AbstractValidator<TextEditDto>
+    {
+        public TextEditValidator()
+        {
+            RuleFor(x => x.NewText).NotEmpty().MaximumLength(500);
+            RuleFor(x => x.X).GreaterThanOrEqualTo(0).LessThanOrEqualTo(10000);
+            RuleFor(x => x.Y).GreaterThanOrEqualTo(0).LessThanOrEqualTo(10000);
+        }
+    }
+}
